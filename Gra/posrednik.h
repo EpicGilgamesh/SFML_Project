5c94523d37deec333ad0f70a1afc80ef1d503092
EpicGilgamesh <77363465+EpicGilgamesh@users.noreply.github.com>
2021-06-10 14:05:02 +0200

    Add files via upload

---
+++ Gra/posrednik.h
@@ -0,0 +1,14 @@
+#ifndef POSREDNIK_H
+#define POSREDNIK_H
+#include<iostream>
+#include"SFML\Graphics.hpp"
+#include"SFML\Window.hpp"
+#include"SFML\System.hpp"
+#include"SFML/Audio.hpp"
+#include<math.h>
+#include<cstdlib>
+#include<vector>
+#include<map>
+using namespace sf;
+
+#endif // POSREDNIK_H
